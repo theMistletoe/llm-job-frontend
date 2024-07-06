ffd9760e165fec2206642b6fc50eea317482ca95
theMistletoe <katachiwotodomete@gmail.com>
2024-07-06 23:53:36 +0900

    refactor: Rename List component to Workers and add selectedWorker state

--- app/routes/workers.tsx
+++ app/routes/workers.tsx
@@ -9,8 +9,9 @@ type Worker = {
     updated_at: string;
 };
 
-export default function List() {
+export default function Workers() {
     const [workers, setWorkers] = useState<Worker[]>([]);
+    const [selectedWorker, setSelectedWorker] = useState<Worker | undefined>(undefined);
     const { getToken } = useAuth();
 
     const getWorkers = useCallback(async () => {
@@ -37,20 +38,26 @@ export default function List() {
         getWorkers();
     }, [getWorkers]);
 
-    return (
+    return !selectedWorker ? (
         <div className="p-5">
             <h1 className="text-2xl font-bold mb-4">List</h1>
             <ul className="space-y-2">
                 {workers.length > 0 ? (
-                    workers.map((worker) => (
-                        <li key={worker.id} className="p-3 bg-gray-100 rounded-md shadow-md">
-                            {worker.script_name}
-                        </li>
+                    workers.map((worker, index) => (
+                        <div tabIndex={index} role="button" key={worker.id} className="p-3 bg-gray-100 rounded-md shadow-md" onClick={() => setSelectedWorker(worker)} onKeyDown={(e) => {
+                            if (e.key === "Enter") setSelectedWorker(worker);
+                            }}>
+                                {worker.script_name}
+                        </div>
                     ))
                 ) : (
                     <li className="p-3 bg-red-100 rounded-md shadow-md">No workers found</li>
                 )}
             </ul>
+        </div>
+    ) : (
+        <div>
+            <h1>{selectedWorker.script_name}</h1>
         </div>
     );
 }
